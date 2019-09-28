9cbbfea61ecb0866130e000e2caf6448d20c3839
Dominick Bruno <dbruno@unseen.is>
2019-09-27 21:46:08 -0400

    Actions and Reducers set up

---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,47 @@
+import { 
+    ERROR, 
+    FETCHING, 
+    FETCHED, 
+    ADDING,
+    ADD
+  } 
+    from "../actions";
+  
+    const initialState = {
+      smurfs: [],
+      fetchingSmurfs: false,
+      addingSmurf: false,
+      // addedSmurf: false,
+      error: null
+    }
+  
+    export const smurfsReducer = (state = initialState, action) => {
+      switch (action.type) {
+        case FETCHING:
+          return { 
+            ...state, 
+            fetchingSmurfs: true };
+        case FETCHED:
+          return {
+            ...state,
+            fetchingSmurfs: false,
+            smurfs: [...state.smurfs, ...action.payload]
+          };
+          case ADDING:
+          return { 
+            ...state, 
+            addingSmurf: true };
+        case ADD:
+          return { 
+            ...state, 
+            addingSmurf: false,
+         
+          }
+        case ERROR:
+          return { 
+            ...state, 
+            error: action.payload };
+        default:
+          return state;
+      }
+    }  
